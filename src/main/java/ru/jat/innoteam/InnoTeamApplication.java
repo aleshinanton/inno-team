525760d3f889256e8567f0e33a9831ad45f34f13
Anton Aleshin <anton.aleshin@gmail.com>
2021-12-02 17:26:20 +0300

    Initial commit

---
+++ src/main/java/ru/jat/innoteam/InnoTeamApplication.java
@@ -0,0 +1,13 @@
+package ru.jat.innoteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnoTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnoTeamApplication.class, args);
+    }
+
+}
