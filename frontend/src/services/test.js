44bce1deaa1223e2db10f8dda13c1242db2d080a
PullDakar <mihailfufaev@mail.ru>
2021-12-02 20:28:04 +0300

    added front-end integration

--- frontend/src/services/test.js
+++ frontend/src/services/test.js
@@ -1,5 +1,5 @@
 export async function getTestData() {
 
-    const response = await fetch('/api/application?page=0&sort=name&sort=mail,asc');
+    const response = await fetch('/api/application?page=0');
     return await response.json();
 }
--- src/main/java/ru/jat/innoteam/controller/ApplicationController.java
+++ src/main/java/ru/jat/innoteam/controller/ApplicationController.java
@@ -4,20 +4,24 @@ import lombok.RequiredArgsConstructor;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.jat.innoteam.model.Application;
 import ru.jat.innoteam.repository.ApplicationRepository;
 
 import javax.validation.constraints.NotNull;
 
+@CrossOrigin
+@RestController
 @RequiredArgsConstructor
-@RestController("/api/application")
+@RequestMapping("/api/application")
 public class ApplicationController {
     private final ApplicationRepository applicationRepository;
 
-    @GetMapping(value = "/", produces = "application/json")
-    public Page<Application> loadApplicationsPage( @NotNull @ParameterObject Pageable pageable) {
+    @GetMapping
+    public Page<Application> loadApplicationsPage(@NotNull @ParameterObject Pageable pageable) {
         return applicationRepository.findAll(pageable);
     }
 }
