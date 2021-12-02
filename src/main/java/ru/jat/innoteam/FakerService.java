b092a1ea1041ce9552ca82f5f3a0339e9b69b4ac
Anton Aleshin <anton.aleshin@gmail.com>
2021-12-02 19:23:35 +0300

    Added python

--- src/main/java/ru/jat/innoteam/FakerService.java
+++ src/main/java/ru/jat/innoteam/FakerService.java
@@ -3,13 +3,15 @@ package ru.jat.innoteam;
 import com.github.javafaker.Faker;
 import org.springframework.stereotype.Service;
 
+import java.util.Locale;
+
 /**
  * Сервис генерации фэйковых данных для завок
  */
 @Service
 public class FakerService {
 
-    private static final Faker FAKER = new Faker();
+    private static final Faker FAKER = new Faker(new Locale("ru"));
 
     public String getTeamName() {
         return FAKER.team().name();
