f5653b23f17e0eaa06a6982bae0f7f62d32d641a
PullDakar <mihailfufaev@mail.ru>
2021-12-02 19:50:25 +0300

    added dependency for springdoc Pageable support

--- src/main/java/ru/jat/innoteam/controller/ApplicationController.java
+++ src/main/java/ru/jat/innoteam/controller/ApplicationController.java
@@ -1,6 +1,7 @@
 package ru.jat.innoteam.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.jat.innoteam.model.Application;
 import ru.jat.innoteam.repository.ApplicationRepository;
 
+import javax.validation.constraints.NotNull;
+
 @RequiredArgsConstructor
 @RestController("/api/application")
 public class ApplicationController {
     private final ApplicationRepository applicationRepository;
 
-    @GetMapping("/")
-    public Page<Application> loadApplicationsPage(Pageable pageable) {
+    @GetMapping(value = "/", produces = "application/json")
+    public Page<Application> loadApplicationsPage( @NotNull @ParameterObject Pageable pageable) {
         return applicationRepository.findAll(pageable);
     }
 }
