cc12604ea197d466167b5437c10518572da771a0
Anton Aleshin <anton.aleshin@gmail.com>
2021-12-04 00:10:49 +0300

    added fulltext to issues

--- src/main/java/ru/jat/innoteam/controller/IssueController.java
+++ src/main/java/ru/jat/innoteam/controller/IssueController.java
@@ -1,10 +1,15 @@
 package ru.jat.innoteam.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+import ru.jat.innoteam.model.Application;
 import ru.jat.innoteam.model.Issue;
 import ru.jat.innoteam.repository.IssueRepository;
 
@@ -38,6 +43,28 @@ public class IssueController {
     @DeleteMapping("/{uuid}")
     public void removeIssueByUuid(@PathVariable("uuid") String uuid) {
         issueRepository.deleteById(UUID.fromString(uuid));
+    }
+
+    @Operation(summary = "Нечеткий поиск")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Выполнено успешно"),
+    })
+    @GetMapping("/search")
+    public Page<Application> fuzzySearch(
+            @Parameter(description = "Текст поиска", example = "текст")
+            @RequestParam String q, Pageable pageable) {
+        return issueRepository.find(q, pageable);
+    }
+
+    @Operation(summary = "Полнотекстовый поиск")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Выполнено успешно"),
+    })
+    @GetMapping("/search/fulltext")
+    public Page<Application> fullTextSearch(
+            @Parameter(description = "Текст поиска", example = "текст")
+            @RequestParam String q, Pageable pageable) {
+        return issueRepository.multiFieldFullTextSearch(q, pageable);
     }
 
 }
